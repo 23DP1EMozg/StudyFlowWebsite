e079b406bef63cb9b8c01d2b498bd8036b2ca2e7
23DP1EMozg <A230240EM@rvt.lv>
2025-05-13 23:47:17 +0300

    bugfix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/StudyFlowWebsite/',
+  base: '/StudyFlowWebsite/', // 👈 THIS IS CRITICAL
   plugins: [react()],
 })
